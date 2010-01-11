8ada72fb6ad54320826616a15a5ec44eaa000f18
Jonas Smedegaard <dr@jones.dk>
2010-01-11 12:56:28 +0100

    Update upstream-tarball.mk to preserve bzip2 tarballs with source format 3.0 (quilt).

--- debian/cdbs/1/rules/upstream-tarball.mk
+++ debian/cdbs/1/rules/upstream-tarball.mk
@@ -107,7 +107,7 @@ get-orig-source:
 	@untar="tar -x -C"; \
 	case "$(cdbs_upstream_received_tarball)" in \
 	    *.tar.gz)  unpack="gunzip -c";; \
-	    *.tar.bz2) unpack="bunzip2 -c";    uncompress="bunzip2";; \
+	    *.tar.bz2) unpack="bunzip2 -c"; grep -q '3.0 (quilt)' debian/source/format || uncompress="bunzip2";; \
 	    *.tar.Z)   unpack="uncompress -c"; uncompress="uncompress";; \
 	    *.zip)     unpack="unzip -q";      uncompress="false";       untar="-d"; nopipe="true";; \
 	    *.tar)     unpack="cat";           uncompress="true";; \
